e4578c2b80b0a0b28f2832ba33c89fd1436a175f
mk <mskk0805@gmail.com>
2020-09-26 17:37:21 +0900

    자산 클래스 생성

--- myproject/finance_manager/models.py
+++ myproject/finance_manager/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Asset(models.Model):
+    """
+    자산 기본 클래스
+    """
+
+    ticker = models.CharField(max_length=10)
+    price = models.FloatField()
+    shares = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        abstract = True
+
+
+class USStock(Asset):
+    pass
+
+
+class KOStock(Asset):
+    pass
+
+
+class Realty(Asset):
+    pass
