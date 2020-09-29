81807f8523423f6c24bc497b42a4ee590cde5362
mk <mskk0805@gmail.com>
2020-09-29 23:57:43 +0900

    asset 클래스 변경 - 외부키  추가 (user) - crud 로그인 조건 추가 - AUTH_USER_MODEL 환경변수 추가

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+AUTH_USER_MODEL = "auth.User"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
