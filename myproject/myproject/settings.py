2819deedd8246cb05b812055bceb5b32803d711d
mk <mskk0805@gmail.com>
2020-10-08 19:08:43 +0900

    add DRF

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "common.apps.CommonConfig",
     "finance_manager.apps.FinanceManagerConfig",
 ]
@@ -77,7 +78,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "myproject.wsgi.application"
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
