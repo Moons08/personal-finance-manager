1a32531859687d200dcbc8bf2cf3c0aed1036695
mk <mskk0805@gmail.com>
2020-10-15 22:36:11 +0900

    폴더명변경

---
+++ backend/finance_manager/views/article_views.py
@@ -0,0 +1,71 @@
+from finance_manager.models.article import Article, Comment
+from finance_manager.serializers.article import ArticleSerializer, CommentSerializer
+
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+
+from . import LargeResultsSetPagination
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LargeResultsSetPagination
+    # @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    # def highlight(self, request, *args, **kwargs):
+    #     article = self.get_object()
+    #     return Response(article.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # http_method_names = ["get", "post", "head"]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+# class ArticleList(generics.ListCreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+
+# class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
+
+
+# @api_view(["GET"])
+# def api_root(request, format=None):
+#     return Response(
+#         {
+#             "users": reverse("user-list", request=request, format=format),
+#             "articles": reverse("article-list", request=request, format=format),
+#             "comments": reverse("coment-list", request=request, format=format),
+#         }
+#     )
