8ade2fb92ca38f94fd5306f29ddd2b8338c11723
DayoungOh <ohdy92@gmail.com>
2020-10-17 18:32:26 +0900

    버튼 컴포넌트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import './App.css';
+import { css } from '@emotion/core';
+import Button from './components/Button/Button';
+import styled, { ThemeProvider } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import LoginBox from './components/LoginBox';
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #fca311;
+  }
+`;
+
+const palette = {
+  primary: '#fca311',
+  secondary: '#14213d'
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <GlobalStyle />    
+    <ThemeProvider theme={{palette}}>
+      <LoginBox>
+        <ButtonGroup>
+          {/* <Button color="secondary" size="large" fullWidth>Enter</Button> */}
+          {/* <Button color="secondary" outline fullWidth>Secondary</Button> */}
+        </ButtonGroup>
+      </LoginBox>
+    </ThemeProvider>
+    </>
   );
 }
 
