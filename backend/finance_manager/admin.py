87dc439f8fab8c4bbc80cf7c41bc350242832fa8
JSPARK113 <32053196+JSPARK113@users.noreply.github.com>
2020-12-05 20:37:44 +0900

    admin 페이지에 pfm에 정의된 모델 추가 (#53)

--- backend/finance_manager/admin.py
+++ backend/finance_manager/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.article import Article, Comment
+from .models.asset import Stock, Realty
+from .models.info import USStockInfo, KOStockInfo
+from .models.portfolio import Portfolio
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    # list_dislay: List 를 정의해 보여질 필드를 정할 수 있음
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    model = Stock
+
+
+@admin.register(Realty)
+class RealtyAdmin(admin.ModelAdmin):
+    model = Realty
+
+
+@admin.register(USStockInfo)
+class USStockInfoAdmin(admin.ModelAdmin):
+    model = USStockInfo
+
+
+@admin.register(KOStockInfo)
+class KOStockInfoAdmin(admin.ModelAdmin):
+    model = KOStockInfo
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    model = Portfolio
