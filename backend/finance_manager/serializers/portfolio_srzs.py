5ad7b4a2d57849819844046602f9d17fd160a159
mk <mskk0805@gmail.com>
2020-12-06 14:46:41 +0900

    portfolio 총 자산 금액 계산값 추가

--- backend/finance_manager/serializers/portfolio_srzs.py
+++ backend/finance_manager/serializers/portfolio_srzs.py
@@ -9,11 +9,31 @@ class PortfolioSerializer(serializers.ModelSerializer):
     user_stocks = UserStockListSerializer(many=True, read_only=True)
     realties = RealtySerializer(many=True, read_only=True)
 
-    def get_total_asset(self, parameter_list):
+    def to_representation(self, obj):
         """
-        docstring
+        총 자산 금액 계산
         """
-        pass
+
+        def _get_total(rep, asset, obj="present_value", to=None):
+            """
+            related asset의 총합 return 값에 추가
+            return: rep += ["{asset}_tot_{to}"]
+            """
+            to = obj if to is None else to
+
+            _assets = rep[asset]
+            rep[f"{asset}_tot_{to}"] = 0
+            for _asset in _assets:
+                rep[f"{asset}_tot_{to}"] += _asset[obj]
+            return rep
+
+        rep = super().to_representation(obj)
+        rep = _get_total(rep, "user_stocks", to="pv")
+        rep = _get_total(rep, "user_stocks", obj="profit")
+        # rep = _get_total(rep, "user_realties")
+        # rep["tot_val"] = rep["user_stocks_tot_val"] + rep["user_realties_tot_val"]
+
+        return rep
 
     class Meta:
         model = Portfolio
