c8d34a5fb54879e746fe6fa7a4c80221ef94c786
mk <mskk0805@gmail.com>
2021-02-03 22:07:41 +0900

    포트폴리오 viewset 수정
    * get_queryset: user가 만든 포트폴리오만 조회 가능, 관리자는 모두 조회 가능
    * list, retieve는 framework 의 viewset 메서드 똑같이 override

--- backend/finance_manager/views/portfolio_views.py
+++ backend/finance_manager/views/portfolio_views.py
@@ -1,9 +1,34 @@
 from . import BaseViewSet
+from django.shortcuts import get_object_or_404
 from finance_manager.models.portfolio import Portfolio
 from finance_manager.serializers.portfolio_srzs import PortfolioSerializer
+from rest_framework.response import Response
 
 
 class PortfolioViewSet(BaseViewSet):
 
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the portfolio
+        for the currently authenticated user.
+        * admin gets all user's portfolio.
+        """
+        user = self.request.user
+        if user.is_superuser:
+            return Portfolio.objects.all()
+        else:
+            return Portfolio.objects.filter(user=user)
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = PortfolioSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Portfolio.objects.all()
+        portfolio = get_object_or_404(queryset, pk=pk)
+        serializer = PortfolioSerializer(portfolio)
+        return Response(serializer.data)
